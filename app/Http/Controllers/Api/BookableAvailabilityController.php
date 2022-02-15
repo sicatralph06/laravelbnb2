06ebbda7ca33d8184a2a61721ce3668748b93c77
Ralph Sicat <ralph.sicat@awsys-i.com>
2022-02-15 16:59:50 +0800

    #53 input date validation

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -15,6 +15,11 @@ class BookableAvailabilityController extends Controller
      */
     public function __invoke(Request $request)
     {
-        dd('Ok');
+        $data = $request->validate([
+            'from' => 'required|date_format:Y-m-d|after_or_equal:now',
+            'to' => 'required|date_format:Y-m-d|after_or_equal:from'
+        ]);
+
+        dd($data);
     }
 }
