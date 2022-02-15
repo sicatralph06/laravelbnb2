f649f5e8a94c06ecee03c2de57e6217516fcc1c6
Ralph Sicat <ralph.sicat@awsys-i.com>
2022-02-15 16:46:01 +0800

    #52 d ko gets

--- routes/api.php
+++ routes/api.php
@@ -30,3 +30,6 @@ Route::get('bookables', 'Api\BookableController@index');
 Route::get('bookables/{id}', 'Api\BookableController@show');
 
 Route::apiResource('bookables', 'Api\BookableController')->only(['index', 'show']);
+
+Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityController')
+    ->name('bookables.availability.show');
