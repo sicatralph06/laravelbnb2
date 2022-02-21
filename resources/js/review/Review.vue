85cc6f54adcaa7884a8cf42beed36cf5dabfa781
Ralph Sicat <ralph.sicat@awsys-i.com>
2022-02-21 14:53:52 +0800

    #77

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,19 +1,33 @@
 <template>
     <div>
-        <div class="form-group">
-            <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
-            <star-rating 
-                :rating="review.rating" 
-                class="fa-3x" 
-                v-on:rating:changed="review.rating = $event"
-            ></star-rating>
-        </div>
-        <div class="form-group">
-            <label for="content" class="text-muted">Describe your experience with</label>
-            <textarea name="content" cols="30" rows="10" class="form-control"></textarea>
-        </div>
+        <div v-if="loading">Loading...</div>
+        <div v-else>
+            <div v-if="alreadyReviewed">
+                <h3>You've already left a review for this booking!</h3>
+            </div>
+            <div v-else>
+                <div class="form-group">
+                    <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
+                    <star-rating 
+                        class="fa-3x" 
+                        v-model="review.rating"
+                    ></star-rating>
+                </div>
+                <div class="form-group">
+                    <label for="content" class="text-muted">Describe your experience with</label>
+                    <textarea 
+                        name="content" 
+                        cols="30" 
+                        rows="10" 
+                        class="form-control"
+                        v-model="review.content"
+                    ></textarea>
+                </div>
 
-        <button class="btn btn-lg btn-primary btn-block">Submit</button>
+                <button class="btn btn-lg btn-primary btn-block">Submit</button>
+            </div>
+        </div>
+       
     </div>
 </template>
 
@@ -24,12 +38,30 @@ export default {
             review: {
                 rating: 5,
                 content: null
-            }
+            },
+            existingReview: null,
+            loading: false
         }
+        
     },
-    methods: {
+    /* methods: {
         onRatingChanged(rating) {
             console.log(rating);
+        }
+    } */
+    created() {
+        this.loading = true;
+        axios
+            .get(`/api/reviews/${this.$route.params.id}`)
+            .then(response => (this.existingReview = response.data.data))
+            .catch(err => {
+
+            })
+            .then(() => (this.loading=false));
+    },
+    computed: {
+        alreadyReviewed() {
+            return this.existingReview != null;
         }
     }
 }
--- resources/js/shared/components/StarRating.vue
+++ resources/js/shared/components/StarRating.vue
@@ -4,7 +4,7 @@
             class="fas fa-star" 
             v-for="star in fullStars" 
             :key="'full' + star" 
-            @click="$emit('rating:changed', star)"
+            @click="$emit('input', star)"
         >
         
         </i>
@@ -13,7 +13,7 @@
             class="far fa-star" 
             v-for="star in emptyStars" 
             :key="'empty' + star" 
-            @click="$emit('rating:changed', fullStars+star)"
+            @click="$emit('input', fullStars+star)"
         ></i>
     </div>
 </template>
@@ -21,21 +21,21 @@
 <script>
 export default {
     props: {
-        rating: Number
+        value: Number
     },
     computed: {
         halfStar() {
             const fraction = Math.round(
-                (this.rating - Math.floor(this.rating)) * 100)
+                (this.value - Math.floor(this.value)) * 100)
             ;
             /* console.log(fraction); */
             return fraction > 0 && fraction < 50;
         },
         fullStars() {
-            return Math.round(this.rating);
+            return Math.round(this.value);
         },
         emptyStars() {
-            return 5 - Math.ceil(this.rating);
+            return 5 - Math.ceil(this.value);
         }
     },
     /* created() {
